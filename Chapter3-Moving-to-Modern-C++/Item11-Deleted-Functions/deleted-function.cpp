5b1177c087ef7956d6ab9e87a6261dcbf2d07d29
Burhan Akkus <burgcanmail@gmail.com>
2026-08-04 21:39:23 +0200

    Item11 - Deleted Functions

---
+++ Chapter3-Moving-to-Modern-C++/Item11-Deleted-Functions/deleted-function.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+bool isLucky(int number);
+bool isLucky(char number) = delete; 
+bool isLucky(double number) = delete; // Will catch floats too. C++ prefers casting floats to double over casting to int 
+bool isLucky(bool number) = delete; 
+
+template<typename T>
+void processPointer(T* ptr);
+
+template<>
+void processPointer<void>(void*) = delete; // processPointer template can't be instantiated with void pointer.
+
+template<>
+void processPointer<char>(char*) = delete;// processPointer template can't be instantiated with char pointer.\
+
+bool isLucky2(int number);
+template<typename T> bool isLucky2(T) = delete; // isLucky2 can't be called with any paramter type except int.
+
+// Other decorators will also need to be removed to be sure :)
+// Combinations of const and volatile.
+// Kinda cumbersome so not written here. Also there should be a better way than doing the 2x2 matrix by hand...
+template<>
+void processPointer<const void>(const void*) = delete;// processPointer template can't be instantiated with const void pointer.
+template<>
+void processPointer<const char>(const char*) = delete;// processPointer template can't be instantiated with copnst char pointer.
+
+
+int main(){
+    //isLucky(true); // error: use of deleted function ‘bool isLucky(bool)’
+    //isLucky(3.5); //error: use of deleted function ‘bool isLucky(double)’
+    //isLucky('a'); //error: use of deleted function ‘bool isLucky(char)
+    float f = 1;
+    //isLucky(f); // error: use of deleted function ‘bool isLucky(double)’
+    isLucky(-1); // Compilation is ok. Fails on Link Time - no definition.
+    unsigned u = 1;
+    //isLucky(u); //error: call of overloaded ‘isLucky(unsigned int&)’ is ambiguous. Picking of function happens first. Then checks if the function is deleted.
+    //isLucky2(u); // error: use of deleted function ‘bool isLucky2(T) [with T = unsigned int]’
+    size_t t = 4;
+    //isLucky(t); //error: call of overloaded ‘isLucky(size_t&)’ is ambiguous.  Picking of function happens first. Then checks if the function is deleted.
+    //isLucky2(t); //error: use of deleted function ‘bool isLucky2(T) [with T = long unsigned int]’
+    const unsigned cu = 4;
+    //isLucky2(cu); //error: use of deleted function ‘bool isLucky2(T) [with T = unsigned int]’
+}
