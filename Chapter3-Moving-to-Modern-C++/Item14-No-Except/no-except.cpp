e32c9d2612bcd8f805f243c3394c141ce0deaf2b
Burhan Akkus <burgcanmail@gmail.com>
2026-08-10 20:32:20 +0200

    noexcept added Benchmark ToDo

--- Chapter3-Moving-to-Modern-C++/Item14-No-Except/no-except.cpp
+++ Chapter3-Moving-to-Modern-C++/Item14-No-Except/no-except.cpp
@@ -7,3 +7,6 @@ int f(int x) noexcept; // no exceptions from f: C++11 style
 // Destruct all local objects and go back to the caller; until the exception is caught. - No exception in this case, only the first parent is needed.
 // C++11 has the guarantee from the developer: No Exceptions will be thrown from this function.
 // It doesn't need the unwindable stack 
+
+// ToDo
+// Benchmark this.
